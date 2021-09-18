5f230ccd9dd20d265ac8903d99bbbdd2f8e03da5
Karin Taliga <karin.taliga@elicit.se>
2021-09-18 14:45:45 +0200

    Add button for loading vehicle list
    Also move app.js code to the correct file

---
+++ scripts/app.js
@@ -0,0 +1,84 @@
+const app = Vue.createApp({
+    // CreateApp takes a configuration object
+    // pure javascript
+
+    // data is a keyword, cannot be named differently
+    // handles our properties/states
+    // has to return an object
+    data() {
+        return {
+            vehicles: [],
+            displayForm: false,
+            displayShowForm: true,
+            regNo: '',
+            make: '',
+            model: '',
+            modelYear: '',
+            mileage: '',
+            imageUrl: '',
+        }
+    },
+    computed: {
+        vehicleName() {
+            // can include conditionals,
+            // calls to REST api:s etc
+            return this.make + ' ' + this.model;
+        },
+    },
+    methods: {
+
+        getVehicleName() {
+            return `(${this.make} - ${this.model})`; 
+        },
+
+        setMake(event) {
+            this.make = event.target.value;
+        },
+
+        onDisplayShowForm() {
+            this.displayShowForm = !this.displayShowForm;
+            this.displayForm = true;
+        },
+
+        addVehicle(event) {
+            event.preventDefault();
+            this.vehicles.push({
+                    regNo: this.regNo,
+                    make: this.make,
+                    model: this.model,
+                    modelYear: this.modelYear,
+                    milage: this.mileage,
+                    imageUrl: 'https://i.postimg.cc/N09fbtKZ/car1.jpg'
+            });
+            this.regNo = '';
+            this.make = '';
+            this.model = '';
+            this.modelYear = '';
+            this.mileage = '';
+        },
+
+        onCancelVehicle(event) {
+            event.preventDefault();
+            this.displayShowForm = !this.displayShowForm;
+            this.displayForm = false;
+        },
+
+        loadAllVehicles() {
+            // vehicles.forEach(vehicle) => {
+	        //    this.vehicles.push(vehicle);
+            // });
+
+            // this.vehicles = vehicles;
+
+            this.vehicles = vehicles.map((vehicle) => {
+                return vehicle;
+            });
+
+            console.log(this.vehicles);
+        }
+
+    }
+
+});
+
+app.mount('#vehicle-admin');

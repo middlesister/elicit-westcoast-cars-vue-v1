eb2e8a2ab5a0aafb6914e1ff74745d845c8704b3
Karin Taliga <karin.taliga@elicit.se>
2021-09-18 11:05:04 +0200

    Bind initial data to our html

--- content/scripts/app.js
+++ content/scripts/app.js
@@ -2,6 +2,19 @@ const app = Vue.createApp({
     // CreateApp takes a configuration object
     // pure javascript
 
+    // data is a keyword, cannot be named differently
+    // handles our properties/states
+    // has to return an object
+    data() {
+        return {
+            regNo: 'MPJ076',
+            make: 'Volkswagen',
+            model: 'Tiguan',
+            modelYear: 2008,
+            mileage: 13500,
+        }
+    }
+
 });
 
 app.mount('#vehicle-admin');
